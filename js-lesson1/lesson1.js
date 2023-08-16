8cfd650495bef407887218f2a16200853bb9a947
Mrkklnk <mrkk1nk1@gmail.com>
2023-08-16 17:51:08 +0200

    added html and js files

---
+++ js-lesson1/lesson1.js
@@ -0,0 +1,50 @@
+// TASK 1.1
+let s1 = 'hello';
+let s2 = 'owu';
+let s3 = 'com';
+let s4 = 'ua';
+
+let n1 = 1;
+let n2 = 10;
+let n3 = -999;
+let n4 = 123;
+let n5 = 3.14;
+let n6 = 2.7;
+let n7 = 16;
+
+let b1 = true;
+let b2 = false;
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+console.log(s4);
+console.log(n1);
+console.log(n2);
+console.log(n3);
+console.log(n4);
+console.log(n5);
+console.log(n6);
+console.log(n7);
+console.log(b1);
+console.log(b2);
+
+// TASK 1.2
+let firstName = 'Marko';
+let middleName = 'Vitaliiovych';
+let lastName = 'Kolinko';
+let full_name = lastName + ' ' + firstName + ' ' + middleName;
+console.log(full_name);
+
+// TASK 1.3
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+// or
+
+let abc = a + b + c;
+console.log(typeof abc);
